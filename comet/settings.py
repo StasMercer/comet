d7eaea92272edb3e7bc88a546c59731d04851306
StasMercer <pro100.stas.ru@gmail.com>
2018-10-21 18:10:22 +0300

    settings changed

--- comet/settings.py
+++ comet/settings.py
@@ -155,8 +155,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework_json_api.pagination.PageNumberPagination',
     'DEFAULT_PARSER_CLASSES': (
-        'rest_framework_json_api.parsers.JSONParser',
-        'rest_framework.parsers.FormParser',
         'rest_framework.parsers.MultiPartParser'
     ),
     'DEFAULT_RENDERER_CLASSES': (
