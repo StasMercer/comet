9f0244ce77cb9fe9a3113a66e1812d2503554755
StasMercer <pro100.stas.ru@gmail.com>
2018-10-21 17:56:45 +0300

    settings changed

--- comet/settings.py
+++ comet/settings.py
@@ -138,7 +138,6 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
         'rest_framework.permissions.AllowAny',
     ),
     'PAGE_SIZE': 10,
