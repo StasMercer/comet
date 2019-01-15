9318fe1774e38eb9dc4ff234597d44855a7daa0c
Mercer <mercerstas@gmail.com>
2019-01-15 21:04:51 +0200

    celery fixed

--- comet/celery.py
+++ comet/celery.py
@@ -9,7 +9,7 @@ from celery import Celery
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'comet.settings')
 
-app = Celery('comet')
+app = Celery('comet', broker='amqp://jm-user1:sample@localhost/jm-vhost')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
